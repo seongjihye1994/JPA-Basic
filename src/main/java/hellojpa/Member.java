23ca8540ac0983bae8bd6dffe1700058bf1b9dca
jihye.seong <jihye.seong@ipageon.com>
2022-04-20 12:54:14 +0900

    @GeneratedValue Identity 전략 특징
    IDENTITY - MYSQL
    IDENTITY 전략은 em.persist() 시점에 즉시 INSERT SQL 실행하고 DB에서 식별자를 조회
    
    SEQUENCE - ORACLE
    TABLE
    AUTO

--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -6,100 +6,34 @@ import java.util.Date;
 @Entity // JPA가 관리할 객체로 지정
 public class Member {
 
+    /**
+     * @GeneratedValue(strategy = GenerationType.AUTO)
+     * DB 방언에 맞게 자동으로 생성됨
+     * ex) oracle -> sequence
+     *
+     * @GeneratedValue(strategy = GenerationType.IDENTITY)
+     * 기본 키 생성을 DB에 위임
+     * ex) mySQL -> AUTO_INCREMENT
+     * 주로 MySQL, PostgreSQL, SQL Server, DB2에서 사용
+     * IDENTITY 전략은 em.persist() 시점에 즉시 INSERT SQL 실행하고 DB에서 식별자를 조회
+     *
+     * @GeneratedValue(strategy = GenerationType.SEQUENCE)
+     * 시퀀스 오브젝트를 만들어냄
+     *  ex) oracle -> sequence
+     *  오라클, PostgreSQL, DB2, H2 데이터베이스에서 사용
+     */
     @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private Long id;
 
-    @Column(name = "name") // 디비 컬럼명은 name
-    private String username; // 객체 필드명은 username
+    private String name;
 
-    private Integer age;
-
-    @Enumerated(EnumType.STRING) // 자바 enum 타입을 매핑할 때 사용, DB 에는 enum 타입이 없으므로, @Enumerated 사용
-    private RoleType roleType;
-
-    @Temporal(TemporalType.TIMESTAMP) // 날짜 타입은 @Temporal 사용
-    private Date createdDate;
-
-    @Temporal(TemporalType.TIMESTAMP)
-    private Date lastModifiedDate;
-
-    @Lob // db 에 varchar 보다 더 큰 컨텐츠를 넣고 싶을 때
-    private String description; // Lob 에 String 이면 clob 타입으로 정의됨
-
-    @Transient // db 에는 반영하지 않고 메모리에서만 사용하고 싶을 때때
-    private int temp;
-
-
-    /**
-     * JPA 는 내부적으로 리플렉션을 쓰기 때문에
-     * 동적으로 객체를 생성한다.
-     * 그래서 커스텀 생성자를 만들때는
-     * 디폴트 생성자도 반드시 기재해줘야 한다.
-     */
     public Member() {
     }
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
+    public Member(Long id, String name) {
         this.id = id;
+        this.name = name;
     }
 
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public Integer getAge() {
-        return age;
-    }
-
-    public void setAge(Integer age) {
-        this.age = age;
-    }
-
-    public RoleType getRoleType() {
-        return roleType;
-    }
-
-    public void setRoleType(RoleType roleType) {
-        this.roleType = roleType;
-    }
-
-    public Date getCreatedDate() {
-        return createdDate;
-    }
-
-    public void setCreatedDate(Date createdDate) {
-        this.createdDate = createdDate;
-    }
-
-    public Date getLastModifiedDate() {
-        return lastModifiedDate;
-    }
-
-    public void setLastModifiedDate(Date lastModifiedDate) {
-        this.lastModifiedDate = lastModifiedDate;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public int getTemp() {
-        return temp;
-    }
-
-    public void setTemp(int temp) {
-        this.temp = temp;
-    }
 }

2efa407bfeaeb7fa24dbf6543a88aa97d55d84b8
jihye.seong <jihye.seong@ipageon.com>
2022-04-21 13:42:27 +0900

    연관관계가 없는 경우와 단방향 연관관계 vs 양방향 연관관계
    객체의 참조 연관관계와 DB의 테이블 조인 연관관계에 대해
    
    연관관계의 주인은 FK를 가지고 있는 테이블이다.
    주인이 아닌 테이블에는 @Mapped(주인FK필드명)을 기재해줘야 한다.
    1:N 관계에서는 N에 FK가 있다.
    그래서 @ManyToOne(연관관계 주인) 과 @Mapped를 같이 사용할 수 없다.
    
    결국엔 양쪽 모두 양방향 연관관계가 되어야 한다.

--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -1,26 +1,26 @@
 package hellojpa;
 
 import javax.persistence.*;
-import java.util.Date;
 
 @Entity // JPA가 관리할 객체로 지정
+@SequenceGenerator(name = "member_seq_generator", sequenceName = "member_seq")
 public class Member {
 
     /**
      * @GeneratedValue(strategy = GenerationType.AUTO)
      * DB 방언에 맞게 자동으로 생성됨
-     * ex) oracle -> sequence
+     *  ex) oracle -> sequence
      *
      * @GeneratedValue(strategy = GenerationType.IDENTITY)
      * 기본 키 생성을 DB에 위임
-     * ex) mySQL -> AUTO_INCREMENT
+     *  ex) mySQL -> AUTO_INCREMENT
      * 주로 MySQL, PostgreSQL, SQL Server, DB2에서 사용
      * IDENTITY 전략은 em.persist() 시점에 즉시 INSERT SQL 실행하고 DB에서 식별자를 조회
      *
      * @GeneratedValue(strategy = GenerationType.SEQUENCE)
      * 시퀀스 오브젝트를 만들어냄
      *  ex) oracle -> sequence
-     *  오라클, PostgreSQL, DB2, H2 데이터베이스에서 사용
+     * 오라클, PostgreSQL, DB2, H2 데이터베이스에서 사용
      */
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
